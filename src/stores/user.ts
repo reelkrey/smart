7e2c90afbdbc2ebd496c002f3bf9c0c8fe08df68
reelkrey <reelkrey@email.com>
2024-01-28 20:41:55 +0000

    add changes

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,6 @@
 import type { IFetch } from '@/common/types/fetch'
 import type { IUser } from '@/common/types/user'
+import type { IUserForm } from '@/common/types/userForm'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import api from '@/services/api'
@@ -18,13 +19,13 @@ export const useUserStore = defineStore('users', () => {
     }
   }
 
-  async function addUser(user: IUser) {
+  async function addUser(user: IUserForm) {
     try {
       await api.post('/users', {
         name: user.first_name,
         job: 'web developer'
       })
-      users.value = [...users.value, user]
+      // users.value = [...users.value, user]
     } catch (error) {
       console.log(error)
     }
